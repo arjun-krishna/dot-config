1d28de35f7aa08951a93de9da36c8cec83c5452d
Arjun Krishna <arjunkris96@gmail.com>
2023-01-16 13:40:53 -0500

    migrate from packer.nvim to lazy.nvim

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  cmd = "Neotree", -- Lazy loads plugin
+  branch = "v2.x",
+  keys = {
+    { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  config = { -- Lazy param load
+    filesystem = {
+      follow_current_file = true,
+      hijack_netrw_behavior = "open_current",
+    },
+  },
+}
