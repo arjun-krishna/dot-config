53d6fb75ecfb706fd5ecc8a4abea4ceee988709c
Arjun Krishna <arjunkris96@gmail.com>
2025-09-13 22:40:11 -0400

    updating installer

--- install.sh
+++ install.sh
@@ -1,74 +1,67 @@
-#!/usr/bin/env bash
-echo "installing .config ..."
+#!/bin/bash
+echo "[.config] installing .config ..."
 
-if [ ! -d "$HOME/.config/nvim" ]; then
-	echo "nvim: creating softlink"
-	ln -s $(pwd)/nvim $HOME/.config/nvim 
-else
-	echo "nvim: already linked"
-fi
+link_config() {
+    local app="$1"
+    local src="$2"
+    local target="$3"
 
-if [ ! -d "$HOME/.config/alacritty" ]; then
-	echo "alacritty: creating softlink"
-    ln -s $(pwd)/alacritty $HOME/.config/alacritty 
-else
-	echo "alacritty: already linked"
-fi
+    if [ -z "$app" ] || [ -z "$src" ] || [ -z "$target" ]; then
+        echo "Usage: link_config <app> <src> <target>"
+        return 1
+    fi
 
-rm -f $(pwd)/alacritty/alacritty.toml
+    if [ -L "$target" ]; then
+        # It's a symlink; check where it points
+        local current_src
+        current_src=$(readlink "$target")
+        if [ "$current_src" != "$src" ]; then
+		echo "[$app] symlink ($target) exists but points to $current_src. Overriding..."
+            rm "$target"
+            ln -s "$src" "$target"
+        else
+            echo "[$app] symlink is already set"
+        fi
+    elif [ -e "$target" ]; then
+        # Exists but is not a symlink
+	echo "[$app] [ERROR] $target exists but is not a symlink. Please remove it or backup manually."
+        return 1
+    else
+        # Doesn't exist; create symlink
+        ln -s "$src" "$target"
+        echo "[$app] created symlink"
+    fi
+}
+update_config() {
+    local app="$1"
+    local src="$2"
+    local target="$3"
+
+    if [ -z "$app" ] || [ -z "$src" ] || [ -z "$target" ]; then
+        echo "Usage: update_config <app> <src> <target>"
+        return 1
+    fi
+    cmp -s "$src" "$target"
+    if [ ! $? -eq 0 ]; then
+	    echo "[$app] updating $target"
+	    cp $src $target
+    else
+	    echo "[$app] already up-to-date."
+    fi
+}
+
+link_config nvim $(pwd)/nvim $HOME/.config/nvim
+link_config tmux $(pwd)/tmux $HOME/.config/tmux
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "alacritty: osx"
-    ln -s $(pwd)/alacritty/osx.toml $(pwd)/alacritty/alacritty.toml
+    link_config alacritty $(pwd)/alacritty/osx $HOME/.config/alacritty
 else
-    echo "alacritty: linux"
-    ln -s $(pwd)/alacritty/linux.toml $(pwd)/alacritty/alacritty.toml
+    link_config alacritty $(pwd)/alacritty/linux $HOME/.config/alacritty
 fi
 
-if [ ! -d "$HOME/.config/tmux" ]; then
-	echo "tmux: creating softlink"
-	ln -s $(pwd)/tmux $HOME/.config/tmux 
-else
-	echo "tmux: already linked"
-fi
+update_config zshrc .zshrc $HOME/.zshrc
+update_config zshrc_custom .zshrc.custom $HOME/.zshrc.custom
+update_config zprofile .zprofile $HOME/.zprofile
+update_config p10k .p10k.zsh $HOME/.p10k.zsh
 
-if [ ! -d "$HOME/.zshrc.custom" ]; then
-	echo "creating .zshrc.custom"
-	ln -s $(pwd)/.zshrc.custom $HOME/.zshrc.custom
-else
-	echo ".zshrc.custom already exists."
-fi
-cmp -s .zshrc $HOME/.zshrc
-if [ ! $? -eq 0 ]; then
-	echo "updating .zshrc"
-	cp .zshrc $HOME/.zshrc
-else
-	echo ".zshrc already up-to-date."
-fi
-cmp -s .zprofile $HOME/.zprofile
-if [ ! $? -eq 0 ]; then
-	echo "updating .zprofile"
-	cp .zprofile $HOME/.zprofile
-else
-	echo ".zprofile already up-to-date."
-fi
-cmp -s .p10k.zsh $HOME/.p10k.zsh
-if [ ! $? -eq 0 ]; then
-	echo "updating p10k configuration"
-	cp .p10k.zsh $HOME/.p10k.zsh
-else
-	echo "p10k already up-to-date."
-fi
-
-cmp -s vim/.vimrc $HOME/.vimrc
-if [ ! $? -eq 0 ]; then
-	echo "updating .vimrc"
-	cp vim/.vimrc $HOME/.vimrc
-else
-	echo ".vimrc already up-to-date."
-fi
-if [ ! -d "$HOME/.vim" ]; then
-	echo "vim: creating softlink"
-	ln -s $(pwd)/vim/.vim $HOME/.vim
-else
-	echo "vim: already linked"
-fi
+update_config vimrc vim/.vimrc $HOME/.vimrc
+link_config vim $(pwd)/vim/.vim $HOME/.vim
