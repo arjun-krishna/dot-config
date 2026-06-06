5cb052b225030a18afd3c1a38a895a5ffda82c60
Arjun Krishna <arjunkris96@gmail.com>
2026-06-06 13:56:01 -0400

    fix: snippet <c-k> -> <c-n> to free up digraphs

--- nvim/lua/lekha/plugins/snippets.lua
+++ nvim/lua/lekha/plugins/snippets.lua
@@ -7,7 +7,7 @@ return {
         local ls = require("luasnip")
         local cmp = require("cmp")
 
-        vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+        vim.keymap.set({"i"}, "<C-N>", function() ls.expand() end, {silent = true})
         vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
         vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
         vim.keymap.set({"i", "s"}, "<C-E>", function()
